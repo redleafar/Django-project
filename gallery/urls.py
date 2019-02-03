3a3874e0ad9e303fa1aae8551fbb54aef008af66
mac <mac@MacBook-Pro-de-mac.local>
2019-02-02 21:31:02 -0500

    Added form

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'agileProject'
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^add/$', views.add_image, name='addImage'),
